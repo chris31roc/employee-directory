ae2d9bffd8309f29c295137aadb2018c9e200db5
chris31roc <chris31roc@yahoo.com>
2021-04-02 14:51:12 -0400

    created API call function using the random user API

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SearchForm from "./components/Search-Form";
+import Thead from "./components/Thead";
+import Tbody from "./components/Tbody"
+
+
+function App() {
+    const [employees, setEmployees] = useState([]);
+    const [ searchTerm, setSearchTerm ] = useState("");
+    const [ sorted, setSorted] = useState(false);
+  
+    //API call to render employees data
+    useEffect(() => {
+        fetch("https://randomuser.me/api/?results=200&nat=us")
+          .then((res) => res.json())
+          .then((json) => {
+            setEmployees(json.results);
+          })
+      
+    }, []);
+}
